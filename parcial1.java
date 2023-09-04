f37d9e27c12d1b1ddff3f6975e5dc7b8e8da90f5
Desinstalar <134030585+Desinstalar@users.noreply.github.com>
2023-09-04 17:04:01 -0500

    Add files via upload

---
+++ parcial1.java
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:context=".SecondFragment">
+
+    <Button
+        android:id="@+id/button_second"
+        android:layout_width="84dp"
+        android:layout_height="55dp"
+        android:text="suma"
+        android:textColorLink="#E91E63"
+        tools:layout_editor_absoluteX="160dp"
+        tools:layout_editor_absoluteY="432dp" />
+
+    <Button
+        android:id="@+id/button"
+        android:layout_width="101dp"
+        android:layout_height="48dp"
+        android:text="resta"
+        android:textColorLink="#CDDC39"
+        tools:layout_editor_absoluteX="153dp"
+        tools:layout_editor_absoluteY="330dp" />
+
+    <Button
+        android:id="@+id/button2"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="division"
+        android:textColor="#673AB7"
+        tools:layout_editor_absoluteX="144dp"
+        tools:layout_editor_absoluteY="233dp" />
+
+    <Button
+        android:id="@+id/button3"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="multiplicacion"
+        tools:layout_editor_absoluteX="153dp"
+        tools:layout_editor_absoluteY="118dp" />
+
+    <Button
+        android:id="@+id/button4"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="resultado"
+        android:textColor="#E91E63"
+        tools:layout_editor_absoluteX="160dp"
+        tools:layout_editor_absoluteY="547dp" />
+</androidx.constraintlayout.widget.ConstraintLayout>
