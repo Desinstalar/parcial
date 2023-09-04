7eacfbe31031bef453bcef07206cd31d524c43d4
Desinstalar <134030585+Desinstalar@users.noreply.github.com>
2023-09-04 17:52:28 -0500

    Add files via upload

--- parcial1.java
+++ parcial1.java
@@ -1,52 +1,22 @@
-<?xml version="1.0" encoding="utf-8"?>
-<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://schemas.android.com/apk/res-auto"
-    xmlns:tools="http://schemas.android.com/tools"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    tools:context=".SecondFragment">
+public class Calculatora {
 
-    <Button
-        android:id="@+id/button_second"
-        android:layout_width="84dp"
-        android:layout_height="55dp"
-        android:text="suma"
-        android:textColorLink="#E91E63"
-        tools:layout_editor_absoluteX="160dp"
-        tools:layout_editor_absoluteY="432dp" />
+    public static double calculate(double operand1, double operand2, char operator) {
+        switch (operator) {
+            case '+':
+                return operand1 + operand2;
+            case '-':
+                return operand1 - operand2;
+            case '*':
+                return operand1 * operand2;
+            case '/':
+                return operand1 / operand2;
+            case '%':
+                return oprand1 % operand2;
+            case '^':
+                return Math.pow(operand1,operand2);
+        }
 
-    <Button
-        android:id="@+id/button"
-        android:layout_width="101dp"
-        android:layout_height="48dp"
-        android:text="resta"
-        android:textColorLink="#CDDC39"
-        tools:layout_editor_absoluteX="153dp"
-        tools:layout_editor_absoluteY="330dp" />
-
-    <Button
-        android:id="@+id/button2"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:text="division"
-        android:textColor="#673AB7"
-        tools:layout_editor_absoluteX="144dp"
-        tools:layout_editor_absoluteY="233dp" />
-
-    <Button
-        android:id="@+id/button3"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:text="multiplicacion"
-        tools:layout_editor_absoluteX="153dp"
-        tools:layout_editor_absoluteY="118dp" />
-
-    <Button
-        android:id="@+id/button4"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:text="resultado"
-        android:textColor="#E91E63"
-        tools:layout_editor_absoluteX="160dp"
-        tools:layout_editor_absoluteY="547dp" />
-</androidx.constraintlayout.widget.ConstraintLayout>
+    
+        return 0;
+    }
+}
